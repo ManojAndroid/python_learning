f7f3ac259a2c68415bb5bc7f628107aa0ee1961d
ManojPrasad Singh <manojprasad_singh@epam.com>
2026-06-09 17:08:59 +0530

    functions example

--- functions.py
+++ functions.py
@@ -5,6 +5,7 @@ def greet(name):
     
 greet("manoj")  # This will print "Hello, manoj!"
 
+print("position arguments example:")
 
 def add(a, b):
     """Returns the sum of a and b."""
@@ -13,3 +14,20 @@ def add(a, b):
 sum=add(2, 3)  # This will return 5
 
 print(sum)  # Output: 5
+
+
+print("default arguments example:")
+
+def addition(a,b=5):
+    """Returns the sum of a and b, with a default value for b."""
+    return a + b
+result=addition(10)  # This will return 15, since b defaults to 5
+print(result)  # Output: 15
+
+print("keyword arguments example:")
+def multiply(x, y):
+    """Returns the product of x and y."""
+    return x * y
+
+product = multiply(y=4, x=3)  # This will return 12, using keyword arguments
+print(product)  # Output: 12
